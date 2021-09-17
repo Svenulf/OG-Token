14ca459a5075c3b2424873627e12a815315da067
Magnus <bostedtmagnus@gmail.com>
2021-09-17 13:09:12 +0200

    updated timelock contract

--- contracts/OGTokenTimelock.sol
+++ contracts/OGTokenTimelock.sol
@@ -1,18 +1,74 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/TokenTimelock.sol";
+import "node_modules/@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract OGTokenTimelock is TokenTimelock {
+/**
+* This contract is an standardized OpenZeppelin contract "TokenTimelock.sol". 
+* No changes has been made to the original contract, except adding input values.
+*/
 
-    address public constant _token = "token address"; // OG Token address
-    uint256 public constant _lockedUpTime = 1 days; // Time added from now (Unix Epoch Time)
-    address public constant _benefactor = 0x4B441a3680e85c375235435963052Aaf977f55BF; // current benefactor = magnusb.eth
+/**
+ * @dev A token holder contract that will allow a beneficiary to extract the
+ * tokens after a given release time.
+ *
+ * Useful for simple vesting schedules like "advisors get all of their tokens
+ * after 1 year".
+ */
+contract TokenTimelock {
+    using SafeERC20 for IERC20;
 
-    constructor () TokenTimelock(
-        IERC20(_token),
-        _benefactor,
-        block.timestamp + _lockedUpTime) {
+    // ERC20 basic token contract being held
+    IERC20 private immutable _token = "token address"; // OG Token address
+
+    // beneficiary of tokens after they are released
+    address private immutable _beneficiary = 0x4B441a3680e85c375235435963052Aaf977f55BF; // current benefactor = magnusb.eth
+
+    // timestamp when token release is enabled
+    uint256 private immutable _releaseTime = 1631959200; // https://www.epochconverter.com/ => lördag 18 september 2021 kl. 12:00:00 GMT+02:00
+
+    constructor(
+        IERC20 token_,
+        address beneficiary_,
+        uint256 releaseTime_
+    ) {
+        require(releaseTime_ > block.timestamp, "TokenTimelock: release time is before current time");
+        _token = token_;
+        _beneficiary = beneficiary_;
+        _releaseTime = releaseTime_;
+    }
+
+    /**
+     * @return the token being held.
+     */
+    function token() public view virtual returns (IERC20) {
+        return _token;
+    }
+
+    /**
+     * @return the beneficiary of the tokens.
+     */
+    function beneficiary() public view virtual returns (address) {
+        return _beneficiary;
+    }
+
+    /**
+     * @return the time when the tokens are released.
+     */
+    function releaseTime() public view virtual returns (uint256) {
+        return _releaseTime;
+    }
+
+    /**
+     * @notice Transfers tokens held by timelock to beneficiary.
+     */
+    function release() public virtual {
+        require(block.timestamp >= releaseTime(), "TokenTimelock: current time is before release time");
+
+        uint256 amount = token().balanceOf(address(this));
+        require(amount > 0, "TokenTimelock: no tokens to release");
+
+        token().safeTransfer(beneficiary(), amount);
     }
 }
