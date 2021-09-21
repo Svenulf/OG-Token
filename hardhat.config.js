e25cfafff7c324151eb9a4035660bf7cc70a085b
Magnus <bostedtmagnus@gmail.com>
2021-09-21 15:42:05 +0200

    updated for deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,15 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
  require('@nomiclabs/hardhat-ethers');
  require('hardhat-deploy');
+ require("@nomiclabs/hardhat-etherscan");
+ const secret = require("./secrets.json")
 
 module.exports = {
   solidity: "0.8.2",
+  networks: {
+    rinkArby: {
+      url: secret.url,
+      accounts: [secret.accountPrivateKey],
+      gasPrice: 30000000
+    }
+  },
 };
