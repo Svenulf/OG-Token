2c7b7c0ae98fff12fa7841f89737fcbade40709c
Magnus <bostedtmagnus@gmail.com>
2021-09-23 10:58:11 +0200

    AUDIT

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,22 @@
- require('@nomiclabs/hardhat-ethers');
- require('hardhat-deploy');
- require("@nomiclabs/hardhat-etherscan");
- const secret = require("./secrets.json")
+require('dotenv').config();
+require('@nomiclabs/hardhat-ethers');
+require('hardhat-deploy');
+require("@nomiclabs/hardhat-etherscan");
+
+ const privateKey = process.env.ACCOUNT_KEY;
+ const rinkArbyKey = process.env.RINKARBY_KEY;
+ const etherscanKey = process.env.ETHERSCAN_KEY;
 
 module.exports = {
   solidity: "0.8.2",
   networks: {
     rinkArby: {
-      url: secret.url,
-      accounts: [secret.accountPrivateKey],
+      url: rinkArbyKey,
+      accounts: [privateKey],
       gasPrice: 30000000
     }
   },
+  etherscan: {
+    apiKey: etherscanKey
+  }
 };
