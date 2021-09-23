390cb4ad360496aed46795ee554d5de0fb68de53
Magnus <bostedtmagnus@gmail.com>
2021-09-23 10:59:02 +0200

    AUDIT

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main () {
+    // We get the contract to deploy
+    const Token = await ethers.getContractFactory('OGToken');
+    console.log('Deploying OG Token...');
+    const token = await Token.deploy();
+    await token.deployed();
+    console.log('OG Token deployed to:', token.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
